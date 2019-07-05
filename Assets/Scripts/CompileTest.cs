ec43168aae6f324a79345870138f79ddc0f9a1c3
Sebastian Lague <sebastlague@gmail.com>
2019-07-05 21:26:15 +0200

    Initial commit

---
+++ Assets/Scripts/CompileTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompileTest : MonoBehaviour {
+
+    public Text input;
+
+    public void Run () {
+        var assembly = Compile (input.text);
+        var method = assembly.GetType ("MyClass").GetMethod ("MyMethod");
+        var del = (Action) Delegate.CreateDelegate (typeof (Action), method);
+        del.Invoke ();
+    }
+
+    public static Assembly Compile (string source) {
+        var options = new CompilerParameters ();
+        options.GenerateExecutable = false;
+        options.GenerateInMemory = true;
+
+        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies ()) {
+            if (!assembly.IsDynamic) {
+                options.ReferencedAssemblies.Add (assembly.Location);
+            }
+        }
+
+        var compiler = new CSharpCompiler.CodeCompiler ();
+        var result = compiler.CompileAssemblyFromSource (options, source);
+
+        foreach (var err in result.Errors) {
+            Debug.Log (err);
+        }
+
+        return result.CompiledAssembly;
+    }
+}
---
+++ Assets/Scripts/Test/CompilerTest.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.CSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompilerTest : MonoBehaviour {
+
+    public Text resultUI;
+    public Text inputUI;
+
+    public void Run () {
+        var compileTimer = new System.Diagnostics.Stopwatch ();
+        var totalTimer = new System.Diagnostics.Stopwatch ();
+        compileTimer.Start ();
+        totalTimer.Start ();
+        string userCode = inputUI.text;
+
+        CSharpCodeProvider provider = new CSharpCodeProvider ();
+        CompilerResults results = provider.CompileAssemblyFromSource (new CompilerParameters (), userCode);
+
+        if (results.Errors.HasErrors) {
+            string errorString = "";
+            foreach (CompilerError error in results.Errors) {
+                errorString += error.ErrorNumber + " " + error.ErrorText + "\n";
+            }
+            resultUI.text = errorString;
+        } else {
+            System.Type classType = results.CompiledAssembly.GetType ("MyClass");
+            System.Reflection.MethodInfo method = classType.GetMethod ("MyFunction");
+
+            object result = method.Invoke (null, null);
+            resultUI.text = result.ToString ();
+        }
+
+        compileTimer.Stop ();
+        resultUI.text += "\nCompile time: " + compileTimer.ElapsedMilliseconds + " ms; Total time: " + totalTimer.ElapsedMilliseconds + " ms.";
+    }
+
+    public object RunCode (string userCode) {
+        CSharpCodeProvider provider = new CSharpCodeProvider ();
+        CompilerResults results = provider.CompileAssemblyFromSource (new CompilerParameters (), userCode);
+
+        System.Type classType = results.CompiledAssembly.GetType ("MyClass");
+        System.Reflection.MethodInfo method = classType.GetMethod ("MyFunction");
+
+        object result = method.Invoke (null, null);
+        return result;
+    }
+
+}
