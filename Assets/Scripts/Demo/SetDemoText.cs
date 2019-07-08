07ccc68caa871f02643c5b57efad32244309a3dd
Sebastian Lague <sebastlague@gmail.com>
2019-07-08 18:14:26 +0200

    Set default input

---
+++ Assets/Scripts/Demo/SetDemoText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetDemoText : MonoBehaviour {
+    public InputField inputField;
+
+    [Multiline (10)]
+    public string defaultInput;
+
+    void Start () {
+        inputField.text = defaultInput;
+    }
+}
